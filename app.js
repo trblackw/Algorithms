5bb1f88aa64b20f9e3cea3e5a1b460ad7f61ba89
Tucker <trblackw@ncsu.edu>
2018-08-07 17:29:44 -0400

    brainstorming which npm libraries will be needed

---
+++ app.js
@@ -0,0 +1,4 @@
+//The goal is to build a REST API using node.js that allows me to access completed algortihm challenge data from edabit.com
+const fs = require('fs');
+const axios = require('axios');
+
--- main.js
+++
@@ -1,1 +0,0 @@
-//The goal is to build a REST API using node.js that allows me to access completed algortihm challenge data from edabit.com

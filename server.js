8cb13e996255b29b0469c9078f2317301cdc6231
Tucker <trblackw@ncsu.edu>
2018-08-09 20:03:07 -0400

    Finished creating base files for project, and connected the css stylesheets (which took some troubleshooting)

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 const express = require("express");
-const hbs = require("hbs"); // handlebars
+const hbs = require("hbs");
 
 const app = express();
 
-app.set('view engine', 'hbs');
-app.use(express.static(__dirname + "/index.hbs"));
+app.set("view engine", "hbs");
+app.use(express.static(__dirname + "/public"));
+
+//logger middleware
+// app.use((req, res, next) => {
+//   let now = new Date().toLocaleTimeString("en-us");
+//   console.log(`${now}: ${req.method}, ${req.url}`);
+//   next();
+// });
 
 app.get("/", (req, res) => {
-    res.render('index.hbs');
-})
+  res.render("index.hbs", {
+    pageTitle: "Algorithms",
+    currentYear: new Date().getFullYear(),
+    testTemplate: "Just testing to make sure handlebars is up and running!",
+  });
+});
 
-
-
-
-app.listen(3000, () => console.log('app running on port 3000!'))
+app.listen(3000, () => console.log("app running on port 3000!"));

c09d49fd02269e6c6f96e681e7baa0862a3ee315
Tucker <trblackw@ncsu.edu>
2018-08-11 15:18:31 -0400

    created and styled footer partial and included hbs helper

--- server.js
+++ server.js
@@ -4,29 +4,31 @@ const hbs = require("hbs");
 const app = express();
 hbs.registerPartials(__dirname + "/views/partials");
 
+hbs.registerHelper('currentYear', () => new Date().getFullYear())
+
 app.set("view engine", "hbs");
 app.use(express.static(__dirname + "/public"));
 
-//logger middleware
-// app.use((req, res, next) => {
-//   let now = new Date().toLocaleTimeString("en-us");
-//   console.log(`${now}: ${req.method}, ${req.url}`);
-//   next();
-// });
+// logger middleware to track requests
+app.use((req, res, next) => {
+  let now = new Date().toLocaleTimeString("en-us");
+  console.log(`${now}: ${req.method}, ${req.url}`);
+  next();
+});
 
+//pages
 app.get("/", (req, res) => {
   res.render("index.hbs", {
     pageTitle: "My relationship with JavaScript",
-    currentYear: new Date().getFullYear(),
   });
 });
 
-app.get('/challenges', (req, res) => {
-  res.render('challenges.hbs');
-})
+app.get("/challenges", (req, res) => {
+  res.render("challenges.hbs");
+});
 
-app.get('/contact', (req, res) => {
-  res.render('contact.hbs');
-})
+app.get("/contact", (req, res) => {
+  res.render("contact.hbs");
+});
 
 app.listen(3000, () => console.log("app running on port 3000!"));
